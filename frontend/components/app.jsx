6f21e3319697fa1a57d720041caf2faeb62879aa
pmin825 <pmin.0825@gmail.com>
2020-10-17 11:50:50 -0700

    Updated structure for product index page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -26,7 +26,7 @@ const App = () => (
       <Switch>
           <AuthRoute exact path="/login" component={LoginFormContainer} /> 
           <AuthRoute exact path="/signup" component={SignupFormContainer} />
-          <Route exact path="/" component={ProductIndexContainer} />
+          {/* <Route exact path="/" component={ProductIndexContainer} /> */}
           <Redirect to="/"></Redirect>
       </Switch>
       <footer>
