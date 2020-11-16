468d84f97e11f4df80efb6842f94a3842d51bee5
pmin825 <pmin.0825@gmail.com>
2020-11-16 08:51:56 -0800

    create bids api controller

---
+++ app/controllers/api/bids_controller.rb
@@ -0,0 +1,24 @@
+class BidsController < ApplicationController
+    def create
+        @bid = Bid.new(bid_params)
+        # debugger
+        if @bid.save
+            render :show
+        else 
+            render json:["Must be signed in to submit a bid"], status: 404
+        end
+    end 
+
+    def destroy
+        @bid = Bid.find_by(id: params[:id])
+        if @bid.destroy
+            render :show 
+        else
+            render json: @bid.errors.full_messages, status: 404
+        end
+    end
+
+    def bid_params
+        params.require(:bid).permit(:bid_amount, :bidder_id, :product_id)
+    end
+end
