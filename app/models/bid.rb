0b4b7533ad57d0140dc7136560def6bd8a762474
pmin825 <pmin.0825@gmail.com>
2020-11-16 08:35:32 -0800

    create model validations and associations for bids

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,2 +1,12 @@
 class Bid < ApplicationRecord
+
+    validates :product_id, :bidder_id, presence: true   
+
+    belongs_to :product,
+        foreign_key: :product_id,
+        class_name: 'Product'
+
+    belongs_to :bidder,
+        foreign_key: :bidder_id, 
+        class_name: 'User'
 end
