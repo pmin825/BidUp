8081b0c0ec6951385bac3e527ad56d943ef01e3a
pmin825 <pmin.0825@gmail.com>
2020-11-16 10:24:27 -0800

    add bids reducer

---
+++ frontend/reducers/bids_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_BIDS, RECEIVE_BID, REMOVE_BID } from '../actions/bid_actions'
+
+const bidsReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({}, state);
+
+    switch (action.type) {
+        case RECEIVE_BIDS:
+            return action.bids 
+        case RECEIVE_BID:
+             nextState[action.bid.id] = action.bid
+             return nextState
+        case REMOVE_BID:
+            delete nextState[action.bidId]
+            return nextState;
+        default:
+            return state;
+    }
+}
+
+export default bidsReducer;
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import productsReducer from "./products_reducer";
+import bidsReducer from "./bids_reducer";
 
 const entitiesReducer = combineReducers({
-  users: usersReducer,
-  products: productsReducer 
+    users: usersReducer,
+    products: productsReducer,
+    bids: bidsReducer
 });
 
 export default entitiesReducer;
